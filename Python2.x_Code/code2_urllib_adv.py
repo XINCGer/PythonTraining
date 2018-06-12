c705e763c6943c7a2fcd5543176965e2bd4fe153
马三小伙儿 <1790617178@qq.com>
2018-06-12 20:59:40 +0800

    urllib库高级用法

---
+++ Python2.x_Code/code2_urllib_adv.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+
+import urllib2
+import urllib
+
+# 设置header agent
+# url = 'http://www.server.com/login'
+# user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+# values = {'username' : 'cqc',  'password' : 'XXXX' }
+# headers = { 'User-Agent' : user_agent }
+# data = urllib.urlencode(values)
+# request = urllib2.Request(url, data, headers)
+# response = urllib2.urlopen(request)
+# page = response.read()
